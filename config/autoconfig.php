d8cd170e1d8a20f38f76065e9c77892d7bfd4226
OpenShift guest <florianchazal@gmail.com>
2013-06-03 10:25:28 -0400

    add autoconfig but need to be tested

---
+++ config/autoconfig.php
@@ -0,0 +1,18 @@
+<?php
+  $services = getenv("VCAP_SERVICES");
+  $services_json = json_decode($services,true);
+  $mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+  
+  $AUTOCONFIG = array(
+  "installed" => false,
+  "adminlogin" => "admin",
+  "adminpass" => "password",
+  "directory" => "./data",
+  "dbtype" => "mysql",
+  "dbname" => $mysql_config["name"],
+  "dbuser" => $mysql_config["user"],
+  "dbpass" => $mysql_config["password"],
+  "dbhost" => $mysql_config["hostname"],
+  "dbtableprefix" => "oc_"
+  );
+  ?>
